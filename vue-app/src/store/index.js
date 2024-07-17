dbaf102a3fec8a6999fdcc211f3feb7bed9f9356
Israt Ahamed Sabbir <93650049+ahamedsabbir@users.noreply.github.com>
2024-07-17 12:34:58 +0600

    laravel package & vue package install

---
+++ vue-app/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    // Your state properties here
+  },
+  mutations: {
+    // Your mutations here
+  },
+  actions: {
+    // Your actions here
+  },
+  getters: {
+    // Your getters here
+  }
+})
